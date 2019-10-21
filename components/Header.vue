6aa010dd350989c12c60397275a07dfc4e71e119
葛西透也 <toyakasai@Toya-Kasai.local>
2019-10-21 13:42:18 +0900

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <p class="title">memo keep app</p>
+  </header>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.header {
+  background-color: #282828;
+  width: 100%;
+  height: 64px;
+  .title {
+    color: #ececec;
+    font-size: 28px;
+    line-height: 64px;
+    font-weight: bold;
+    margin: 0;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "~/components/Header.vue";
+import Memo from "~/components/Memo.vue";
+
+export default {
+  components: {
+    Header,
+    Memo
+  },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
